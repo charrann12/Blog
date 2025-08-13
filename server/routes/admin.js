bc8273432df7da7a84f0fa5ef8dac061c7faed0c
charrann12 <nethisaicharan12345@gmail.com>
2025-08-13 21:01:00 +0530

    edit and delete option implementation

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -105,11 +105,12 @@ router.get('/dashboard', authMiddleware, async(req, res)=>{
     }
     try{
         const data  = await Post.find();
-        res.render('admin/dashboard',{
+        res.render("admin/edit-post", {
             locals,
             data,
             layout: adminLayout
-        })
+        });
+        
 
     }catch(error){
         console.log(error);
@@ -176,8 +177,52 @@ router.post('/add-post', authMiddleware, async(req, res)=>{
 
 
 
+/*
+GET/
+Admin - Edit a Post 
+*/
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+    try {
+        const locals = {
+            title: "Edit Post",
+            description: "Simple blog"
+        };
+
+        const data = await Post.findOne({ _id: req.params.id });
+
+        res.render("admin/edit-post", {
+            locals,
+            data,
+            layout: adminLayout
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
+
+
+/*
+PUT/
+Admin - Edit a Post 
+*/
+
+router.put('/edit-post/:id', authMiddleware, async(req, res)=>{
+    
+    try{
+
+        await Post.findByIdAndUpdate(req.params.id, {
+            title:req.body.title,
+            body:req.body.body,
+            updatedAt: Date.now()
+        });
+
+        res.redirect(`/edit-post/${req.params.id}`);
+    }catch(error){
+        console.log(error);
+    }
+});
 
 
 
